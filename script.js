a3a838a32bd472d06c8f39ad6e58690e5fc7ea4a
aaa-ivan <ivan.m@aaafreight.net>
2023-05-20 05:29:25 -0500

    Update

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ window.onload = function() {
   if (detectMobile()) {
       var message = document.createElement('div');
       message.innerHTML = 'This game is not available on mobile devices. Please visit from a desktop or laptop to play or visit our <a href="https://www.aaafreight.net">Go to the website</a>!';
-      message.style.cssText = 'position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-image: url('images/bg.png'); color: #fff; padding: 20px; text-align: center; font-size: 24px; z-index: 9999;';
+      message.style.cssText = "position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-image: url('images/bg.png'); color: #fff; padding: 20px; text-align: center; font-size: 24px; z-index: 9999;";
       document.body.style.overflow = 'hidden'; // Prevent scrolling
       document.body.appendChild(message);
   } else {
